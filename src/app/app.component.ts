6939f05f37c6ecf8227750d2e0323bfefa7fc8f9
axewhyzed <coding0234@gmail.com>
2024-02-24 11:22:40 +0530

    Created editor and added features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -11,4 +11,43 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'text-format-app';
+
+  isBold = false;
+  isItalic = false;
+  isUnderline = false;
+  isStrikethrough = false;
+  currentFontSize = '14px';
+
+  @ViewChild('editableDiv') editableDiv!: ElementRef;
+
+  toggleFormat(format: string): void {
+    switch (format) {
+      case 'bold':
+        this.isBold = !this.isBold;
+        break;
+      case 'italic':
+        this.isItalic = !this.isItalic;
+        break;
+      case 'underline':
+        this.isUnderline = !this.isUnderline;
+        break;
+      case 'strikethrough':
+        this.isStrikethrough = !this.isStrikethrough;
+        break;
+    }
+    document.execCommand(format);
+  }
+
+  changeTextColor(color: string): void {
+    document.execCommand('foreColor', false, color);
+  }
+
+  changeFontSize(fontSize: string): void {
+    this.currentFontSize = fontSize;
+    document.execCommand('fontSize', false, fontSize);
+  }
+
+  changeFontFamily(fontFamily: string): void {
+    document.execCommand('fontName', false, fontFamily);
+  }
 }
